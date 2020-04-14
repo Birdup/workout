0399dd31bbd3939b7fa584e95cf161f055a27cd8
Birdup <Wesley313@gmail.com>
2020-04-13 22:11:31 -0400

    saved

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import random
 import os
-os.chdir("workout")
+#os.chdir("workout")
 from wo_list import wo_list
 from wo_weight import wo_weight
 from wo_list import run_list
+from datetime import date
 
 
 def wo_picker():
-    random.randrange(1,100)
-    return random.choices(
+    wos = random.choices(
         wo_list,
         weights=wo_weight,
         k=random.randrange(8,12)
     )
+    if random.randrange(1,100) > 90:
+        wos.append(random.choice(run_list))
+    return wos
+
+#make file with workout
+with open('WOD.txt', 'w') as filehandle:
+    filehandle.write(str(date.today()))
+    filehandle.write('\n')
+    filehandle.write("Number of workouts today - ")
+    filehandle.write(str(len(wo_picker())))
+    filehandle.write('\n')
+    filehandle.write('#############################################\n')
+    for item in wo_picker():
+        filehandle.write('%s\n' % item)
 
 print("number of workouts today - ", len(wo_picker()))
 print(wo_picker())
